073b2fd37768b59d09ef011505d388f5f2174b75
Davis <davisharder@gmx.de>
2022-10-30 14:55:46 +0100

    Initial commit

---
+++ src/main/java/me/newdavis/stack/Stack.java
@@ -0,0 +1,17 @@
+package me.newdavis.stack;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Stack extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
